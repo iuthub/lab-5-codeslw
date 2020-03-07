dea2e347dbda70599f82892c027b4c4cd2ae6dd7
codeslw <sanvarkhujaev@gmail.com>
2020-03-07 23:45:29 +0500

    Last commit

--- webpage/sucker.php
+++ webpage/sucker.php
@@ -1,3 +1,12 @@
+<?php
+if (isset($_POST['submit'])){
+	$name=$POST['name'];
+	$section=$POST['section'];
+	$creditcardnumber=$POST['credit card'];
+	$radio=$POST['Visa'];
+	$radio2=$POST['Master card'];
+	$valid==(empty($section) or empty($name) or empty($creditcardnumber) or (empty($radio) and empty($radio2)))?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
@@ -6,7 +15,7 @@
 	</head>
 	
 	<body>
-	
+		
 
 		<h1>Buy Your Way to a Better Education!</h1>
 
@@ -15,35 +24,38 @@
 		</p>
 		
 		<hr />
-		<form action="localhostlearning/lab-5-codeslw/webpage/sucker.php" method="POST">
+		
 
 		<h2>Give Us Your Money</h2>
+		<?php 
+		if(!valid){
+			?>
 		<dl>
 			<dt>Name</dt>
 			<dd>
-               <input type="text" name="name" />
+              <?= $name ?>
 			</dd>
 			
 			<dt>Section</dt>
 			<dd>
-				<select name="select section" id="section">
-					<option value="Tashkent">Tashkent</option>
-					<option value="Incheon">Incheon</option>
-				</select>
+				<?= $section ?>
 			</dd>
 			
 			<dt>Credit Card</dt>
 			<dd>
-				<input type="text" name="creditcardnumber" />
-			</br>
-				<p><input type="radio" name="visa">Visa
-				<input type="radio" name="Master Card" />Master Card</p>
+				<?= $creditcardnumber ?>
+				</br>
+				<?= $radio ?>
 			</dd>
 		</dl>
 		
 		<div>
-			<input type="submit" name="submit"value="I am a giant sucker." /> 
+			
 		</div>
-		</form>
+	<?php } 
+	else{?>
+		<p>Please feel out all the forms!</p>
+		 <a href="buyagrade.php">Try again?</a></p>
+		
 	</body>
 </html>
